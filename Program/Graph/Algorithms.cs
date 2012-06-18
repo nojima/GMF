b06276d357774c513b97ac19a00355c3fbbad6e4
Yusuke Nojima <nojima@kmc.gr.jp>
2012-06-18 18:51:00 +0900

    Contract追加

--- Program/Graph/Algorithms.cs
+++ Program/Graph/Algorithms.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Graph {
     public static class Algorithms {
@@ -42,6 +43,33 @@ namespace Graph {
                 currId += 1;
             }
             return currId;
+        }
+
+        /// <summary>
+        /// グラフを mapping に従って縮約したものを返す．
+        /// </summary>
+        /// <param name="graph">縮約するグラフ</param>
+        /// <param name="mapping">mapping[i] は 縮約元のグラフのi番目の頂点の縮約先の頂点</param>
+        /// <returns>縮約されたグラフ</returns>
+        public static DirectedGraph Contract(DirectedGraph graph, int[] mapping) {
+            var result = new DirectedGraph();
+            int nVertices = mapping.Max() + 1;
+            for (int i = 0; i < nVertices; ++i) {
+                result.AddVertex(new Vertex());
+            }
+            for (int i = 0; i < mapping.Length; ++i) {
+                result.Vertices[mapping[i]] = graph.Vertices[i];    // copy vertex
+            }
+            for (int u = 0; u < graph.Vertices.Count; ++u) {
+                int src = mapping[u];
+                foreach (Edge e in graph.OutEdges[u]) {
+                    int dst = mapping[e.Dst];
+                    if (src != dst) {
+                        result.AddEdge(new Edge(src, dst));
+                    }
+                }
+            }
+            return result;
         }
     }
 }
