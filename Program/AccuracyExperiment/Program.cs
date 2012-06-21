3b13ace3246747133561701915c1eb7aa83efefd
Yusuke Nojima <nojima@kmc.gr.jp>
2012-06-21 21:31:53 +0900

    GMFの計算をn回やるコードを書いた

---
+++ Program/AccuracyExperiment/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
+using Graph;
+using CommandLine;
+using CommandLine.Text;
+using GeneralizedMaximumFlow;
+
+namespace AccuracyExperiment {
+    class Options {
+        [Option(null, "input", Required = true, HelpText = "入力元のファイル名")]
+        public string Input { get; set; }
+
+        [Option(null, "output", Required = true, HelpText = "出力先のディレクトリ名")]
+        public string Output { get; set; }
+
+        [Option(null, "eps", Required = true, HelpText = "許容誤差")]
+        public double Eps { get; set; }
+
+        [Option(null, "testcase", Required = true, HelpText = "テストを何回行うか")]
+        public int TestCase { get; set; }
+
+        [Option(null, "quiet", HelpText = "トレースメッセージを出力しない")]
+        public bool Quiet { get; set; }
+
+        [HelpOption]
+        public string GetUsage() {
+            var help = new HelpText {
+                AddDashesToOption = true,
+                Heading = "Usage: AccuracyExperiment [OPTIONS]",
+            };
+            help.AddPostOptionsLine("詳しくは ReadMe.md を参照．\n");
+            help.AddOptions(this);
+            return help;
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            var options = InitializeOptions(args);
+
+            Trace.WriteLine("Loading graph from " + options.Input);
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var graph = GraphIO.LoadCSV(options.Input);
+            var graphLoadTime = stopwatch.ElapsedMilliseconds;
+            stopwatch.Stop();
+
+            int n = graph.Vertices.Count;
+            int m = graph.Edges.Count;
+            var cap = new double[m];
+            var gain = new double[m];
+
+            // とりあえず cap と gain は一つの値で埋める
+            for (int e = 0; e < m; ++e) {
+                cap[e] = 1.0;
+                gain[e] = 0.5;
+            }
+
+            var random = new Random();
+            for (int i = 0; i < options.TestCase; ++i) {
+                int s, t;
+                do {
+                    s = random.Next(n);
+                    t = random.Next(n);
+                } while (s == t);
+                var output = string.Format("{0}/{1}", options.Output, i);
+                GMF.Run(options.Input, output, graphLoadTime, graph, cap, gain, s, t, options.Eps);
+            }
+        }
+
+        private static Options InitializeOptions(string[] args) {
+            var options = new Options();
+            if (!CommandLineParser.Default.ParseArguments(args, options)) { Environment.Exit(1); }
+
+            if (!options.Quiet) {
+                Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+                Trace.AutoFlush = true;
+            }
+            return options;
+        }
+    }
+}
--- Program/GeneralizedMaximumFlow/Program.cs
+++ Program/GeneralizedMaximumFlow/Program.cs
@@ -38,7 +38,47 @@ namespace GeneralizedMaximumFlow {
         }
     }
 
-    class Program {
+    public static class GMF {
+        /// <summary>
+        /// GMFを計算して結果を出力する
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="output"></param>
+        /// <param name="graphLoadTime"></param>
+        /// <param name="graph"></param>
+        /// <param name="cap"></param>
+        /// <param name="gain"></param>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <param name="eps"></param>
+        public static void Run(string input, string output, long graphLoadTime, DirectedGraph graph, double[] cap, double[] gain, int s, int t, double eps) {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var flow = new double[graph.Edges.Count];
+            double value = FleischerWayne.GeneralizedMaximumFlow(graph, cap, gain, s, t, eps, out flow);
+            var gmfTime = stopwatch.ElapsedMilliseconds;
+            stopwatch.Stop();
+
+            Trace.WriteLine("Writing the results");
+            Utility.CreateDirectoryIfNotExists(output);
+            using (var writer1 = new StreamWriter(output + "/Flow.csv")) {
+                OutputFlow(writer1, graph, cap, gain, flow);
+            }
+            using (var writer2 = new StreamWriter(output + "/Value.txt")) {
+                writer2.WriteLine(value);
+            }
+            using (var writer3 = new StreamWriter(output + "/Summary.txt")) {
+                writer3.WriteLine("Input: " + input);
+                writer3.WriteLine("Vertex Count: " + graph.Vertices.Count);
+                writer3.WriteLine("Edge Count: " + graph.Edges.Count);
+                writer3.WriteLine("Source: " + graph.Vertices[s].OriginalId);
+                writer3.WriteLine("Sink: " + graph.Vertices[t].OriginalId);
+                writer3.WriteLine("Eps: " + eps);
+                writer3.WriteLine("Graph Load Time [ms]: " + graphLoadTime);
+                writer3.WriteLine("GMF Time [ms]: " + gmfTime);
+            }
+        }
+
         static void OutputFlow(StreamWriter writer, DirectedGraph graph, double[] cap, double[] gain, double[] flow) {
             for (int e = 0; e < graph.Edges.Count; ++e) {
                 writer.WriteLine("{0},{1},{2},{3},{4}",
@@ -47,17 +87,20 @@ namespace GeneralizedMaximumFlow {
                     cap[e], gain[e], flow[e]);
             }
         }
+    }
 
+    class Program {
         static void Main(string[] args) {
-            var stopwatch = new Stopwatch();
             var options = InitializeOptions(args);
             Trace.WriteLine("Input: " + options.Input);
             Trace.WriteLine("Output: " + options.Output);
             
             Trace.WriteLine("Loading the graph from " + options.Input);
+            var stopwatch = new Stopwatch();
             stopwatch.Start();
             DirectedGraph graph = GraphIO.LoadCSV(options.Input);
             var graphLoadTime = stopwatch.ElapsedMilliseconds;
+            stopwatch.Stop();
 
             int n = graph.Vertices.Count;
             int m = graph.Edges.Count;
@@ -79,29 +122,7 @@ namespace GeneralizedMaximumFlow {
             }
 
             Trace.WriteLine("Caluculating the generalized maximum flow");
-            stopwatch.Restart();
-            var flow = new double[m];
-            double value = FleischerWayne.GeneralizedMaximumFlow(graph, cap, gain, s, t, options.Eps, out flow);
-            var gmfTime = stopwatch.ElapsedMilliseconds;
-
-            Trace.WriteLine("Writing the results");
-            Utility.CreateDirectoryIfNotExists(options.Output);
-            using (var writer1 = new StreamWriter(options.Output + "/Flow.csv")) {
-                OutputFlow(writer1, graph, cap, gain, flow);
-            }
-            using (var writer2 = new StreamWriter(options.Output + "/Value.txt")) {
-                writer2.WriteLine(value);
-            }
-            using (var writer3 = new StreamWriter(options.Output + "/Summary.txt")) {
-                writer3.WriteLine("Input: " + options.Input);
-                writer3.WriteLine("Vertex Count: " + n);
-                writer3.WriteLine("Edge Count: " + m);
-                writer3.WriteLine("Source: " + options.Source);
-                writer3.WriteLine("Sink: " + options.Sink);
-                writer3.WriteLine("Eps: " + options.Eps);
-                writer3.WriteLine("Graph Load Time [ms]: " + graphLoadTime);
-                writer3.WriteLine("GMF Time [ms]: " + gmfTime);
-            }
+            GMF.Run(options.Input, options.Output, graphLoadTime, graph, cap, gain, s, t, options.Eps);
 
             Trace.WriteLine("Done");
         }
