6570b8057aa1c3c3bc7c90850116a7b91ef4a232
Yusuke Nojima <nojima@kmc.gr.jp>
2012-09-08 21:56:59 +0900

    Imprement ConstructInitialFlow

--- Program/GeneralizedMaximumFlow/GreedyGMF.cs
+++ Program/GeneralizedMaximumFlow/GreedyGMF.cs
@@ -38,7 +38,6 @@ namespace GeneralizedMaximumFlow {
                     if (v == t) { break; }
 
                     foreach (Edge e in graph.OutEdges[v]) {
-                        Debug.Assert(cap[e.Index] - flow[e.Index] > -1e-8);
                         if (cap[e.Index] - flow[e.Index] <= 1e-10) { continue; }
                         int w = e.Dst;
                         double newIncome = Math.Min(income[v], cap[e.Index] - flow[e.Index]) * gain[e.Index];
@@ -82,6 +81,63 @@ namespace GeneralizedMaximumFlow {
             return value;
         }
 
+        public static void ConstructInitialFlow(DirectedGraph graph, double[] cap, double[] gain, int s, int t, out double[] flow) {
+            int n = graph.Vertices.Count;
+            int m = graph.Edges.Count;
+
+            var prev = new Edge[n];
+            var income = new double[n];
+            var queue = new PriorityQueue<State>();
+
+            income[s] = Infty;
+            queue.Enqueue(new State(Infty, s, new Edge(s, s)));
+
+            while (queue.Count > 0) {
+                State state = queue.Dequeue();
+                int v = state.Vertex;
+                if (prev[v] != null) { continue; }
+                prev[v] = state.Edge;
+
+                foreach (Edge e in graph.OutEdges[v]) {
+                    int w = e.Dst;
+                    double newIncome = income[v] * gain[e.Index];
+                    if (newIncome > income[w]) {
+                        income[w] = newIncome;
+                        queue.Enqueue(new State(newIncome, w, e));
+                    }
+                }
+            }
+
+            var cap2 = new double[m];
+            foreach (var e in graph.Edges) {
+                cap2[e.Index] = cap[e.Index] / income[e.Src];
+            }
+
+            var flow2 = new double[m];
+            Dinic.MaximumFlow(graph, cap2, s, t, out flow2);
+
+            flow = new double[m];
+            var visit = new bool[n];
+            Translate(graph, flow2, flow, visit, t);
+        }
+
+        private static void Translate(DirectedGraph graph, double[] flow, double[] gflow, bool[] visit, int v) {
+            visit[v] = true;
+            double x = 0, y = 0;
+            foreach (var e in graph.InEdges[v]) {
+                if (flow[e.Index] > 1e-10) {
+                    if (!visit[e.Src])
+                        Translate(graph, flow, gflow, visit, e.Src);
+                    x += flow[e.Index];
+                    y += gflow[e.Index];
+                }
+            }
+            foreach (var e in graph.OutEdges[v]) {
+                if (flow[e.Index] > 1e-10)
+                    gflow[e.Index] = (x < 1e-10) ? flow[e.Index] : y * flow[e.Index] / x;
+            }
+        }
+ 
         struct State : IComparable<State> {
             public double Income;
             public int Vertex;
